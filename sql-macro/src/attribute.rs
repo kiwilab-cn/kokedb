0631460988b9693ce2b01e91a3d49c3b5b122452
zery <zerix.os@gmail.com>
2025-08-27 22:18:21 +0800

    added sql parser function.

--- sql-macro/src/attribute.rs
+++ sql-macro/src/attribute.rs
@@ -1,7 +1,12 @@
 use std::mem;
 
-use syn::{punctuated::Punctuated, spanned::Spanned, Attribute, Meta, MetaNameValue, Token};
+use syn::punctuated::Punctuated;
+use syn::spanned::Spanned;
+use syn::{Attribute, Expr, Meta, MetaNameValue, Path, Token};
 
+/// An extractor for a specific attribute name.
+/// The attribute can have named arguments such as `#[attribute(argument = value)]`,
+/// or paths such as `#[attribute(path)]`.
 pub struct AttributeExtractor<'a> {
     name: &'a str,
     arguments: Vec<MetaNameValue>,
@@ -9,36 +14,44 @@ pub struct AttributeExtractor<'a> {
 }
 
 impl<'a> AttributeExtractor<'a> {
+    /// Creates an extractor for the given attribute name.
+    /// The arguments and paths are collected from the attribute list and
+    /// stored in the extractor for further extraction.
     pub fn try_new(name: &'a str, attributes: &[Attribute]) -> syn::Result<Self> {
         let mut arguments = Vec::new();
         let mut paths = Vec::new();
-
         for attr in attributes {
             if !attr.path().is_ident(name) {
                 continue;
             }
-
             let nested = attr.parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)?;
             for meta in nested {
                 match meta {
-                    Meta::Path(path) => paths.push(path),
-                    Meta::List(_meta_list) => {
-                        return Err(syn::Error::new(meta.span(), "invalid attribute value"))
+                    Meta::Path(x) => {
+                        paths.push(x);
                     }
-                    Meta::NameValue(meta_name_value) => arguments.push(meta_name_value),
+                    Meta::NameValue(x) => {
+                        arguments.push(x);
+                    }
+                    _ => return Err(syn::Error::new(meta.span(), "invalid attribute value")),
                 }
             }
-
-            Ok(Self {
-                name,
-                arguments,
-                paths,
-            })
         }
+        Ok(Self {
+            name,
+            arguments,
+            paths,
+        })
     }
 
-    pub fn extract_empty(&self) -> syn::Result<()> {
+    /// Returns an error if there are any remaining arguments or paths for the attribute.
+    pub fn expect_empty(&self) -> syn::Result<()> {
         if let Some(x) = self.arguments.first() {
+            Err(syn::Error::new(
+                x.span(),
+                format!("unexpected `{}` attribute argument", self.name),
+            ))
+        } else if let Some(x) = self.paths.first() {
             Err(syn::Error::new(
                 x.span(),
                 format!("unexpected `{}` attribute path", self.name),
@@ -48,17 +61,19 @@ impl<'a> AttributeExtractor<'a> {
         }
     }
 
+    /// Extracts a single argument value from the attribute.
+    /// The argument is removed from the extractor.
+    /// Returns an error if there are multiple arguments with the same name.
     pub fn extract_argument_value<T, F>(&mut self, argument: &str, transform: F) -> syn::Result<T>
     where
         F: FnOnce(Option<Expr>) -> syn::Result<T>,
     {
-        let argments = mem::take(&mut self.arguments);
-        let (mut extracted, remaining) = argments
+        let arguments = mem::take(&mut self.arguments);
+        let (mut extracted, remaining) = arguments
             .into_iter()
             .partition::<Vec<_>, _>(|x| x.path.is_ident(argument));
         self.arguments = remaining;
         let one = extracted.pop();
-
         if let Some(other) = extracted.last() {
             Err(syn::Error::new(
                 other.span(),
@@ -72,13 +87,15 @@ impl<'a> AttributeExtractor<'a> {
         }
     }
 
+    /// Extracts a single path from the attribute.
+    /// The path is removed from the extractor.
+    /// Returns an error if there are multiple paths with the same name.
     #[allow(unused)]
     pub fn extract_path(&mut self, path: &str) -> syn::Result<Option<()>> {
         let paths = mem::take(&mut self.paths);
         let (mut extracted, remaining) = paths
             .into_iter()
             .partition::<Vec<_>, _>(|x| x.is_ident(path));
-
         self.paths = remaining;
         let one = extracted.pop();
         if let Some(other) = extracted.last() {
--- sql-macro/src/utils.rs
+++ sql-macro/src/utils.rs
@@ -1,4 +1,6 @@
-use syn::{parse::Parser, ExprLit};
+use syn::parse::Parser;
+use syn::spanned::Spanned;
+use syn::{Expr, ExprLit, Lit};
 
 pub fn parse_string_value<F: Parser>(e: &Expr, parser: F) -> syn::Result<F::Output> {
     let Expr::Lit(ExprLit {
@@ -10,13 +12,11 @@ pub fn parse_string_value<F: Parser>(e: &Expr, parser: F) -> syn::Result<F::Outp
             "the value must be a string literal",
         ));
     };
-
     if !lit.suffix().is_empty() {
         return Err(syn::Error::new(
             lit.span(),
             "the value cannot have a suffix",
         ));
     }
-
     lit.parse_with(parser)
 }
